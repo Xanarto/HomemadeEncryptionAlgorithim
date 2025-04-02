d193cdebfe6c96e08793b5a738be10e53e145c55
Xanarto <the.one.and.only.xanarto@gmail.com>
2025-04-02 21:56:26 +0800

    Fixed importing issues

--- homeMadeEncryptionAlgorithim.py
+++ homeMadeEncryptionAlgorithim.py
@@ -1,9 +1,10 @@
 import base64
 
-e = "Xanarto's_0wn_C1ph3r_T0_3ncrypt_Stuff_H1ms3lf!,.EH7sg"
+_test_message = "Xanarto's_0wn_C1ph3r_T0_3ncrypt_Stuff_H1ms3lf!,.EH7sg"
 
 def encrypt(message : str):
     z = ([ord(str(x)) for x in base64.b64encode(message.encode('utf-8')).decode('utf-8')] + [ord("z")])[::-1]
-    return "".join(map(str,[chr(x * 3) for x in [x + y for (x,y) in (zip(z[::2], z[1::2]))]]))
+    return "".join(map(str,[chr(x * 3) for x in [str(x) + str(y) for (x,y) in (zip(z[::2], z[1::2]))]]))
 
-print(encrypt(e))
+if __main__ == "__name__":
+    print(encrypt(test_message))
