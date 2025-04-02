0449a9f33be7a9dfa3d7ce54d78955f5fd845526
Xanarto <the.one.and.only.xanarto@gmail.com>
2025-04-02 21:54:37 +0800

    removed confidential info

--- homeMadeEncryptionAlgorithim.py
+++ homeMadeEncryptionAlgorithim.py
@@ -1,6 +1,6 @@
 import base64
 
-e = "pecan{The_Xanarto"
+e = "Xanarto's_0wn_C1ph3r_T0_3ncrypt_Stuff_H1ms3lf!,.EH7sg"
 
 def encrypt(message : str):
     z = ([ord(str(x)) for x in base64.b64encode(message.encode('utf-8')).decode('utf-8')] + [ord("z")])[::-1]
