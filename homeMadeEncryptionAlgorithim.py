d5b4eefa321a78f363546c5153e1b04188e56f88
Xanarto <the.one.and.only.xanarto@gmail.com>
2025-05-09 19:23:19 +0800

    Fixed algorithm

--- homeMadeEncryptionAlgorithim.py
+++ homeMadeEncryptionAlgorithim.py
@@ -3,7 +3,7 @@ import base64
 _test_message = "Xanarto's_0wn_C1ph3r_T0_3ncrypt_Stuff_H1ms3lf!,.EH7sg"
 
 def encrypt(message : str):
-    z = ([ord(str(x)) for x in base64.b64encode(message.encode('utf-8')).decode('utf-8')] + [ord("z")])[::-1]
+    z = ([ord("z")] + [ord(str(x)) for x in base64.b64encode(message.encode('utf-8')).decode('utf-8')])[::-1]
     return "".join(map(str,[chr(x * 2) for x in [int(str(x) + str(y)) for (x,y) in (zip(z[::2], z[1::2]))]]))
 
 if __main__ == "__name__":
